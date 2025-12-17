68992e47f512b36cdef633302ab41826412473ab
Pit Buttchereit <pit.buttchereit@student.hpi.uni-potsdam.de>
2025-12-17 12:39:00 +0100

    refactor bindingUtilsNew

--- lib/simulation/bindingUtilsNew.ts
+++ lib/simulation/bindingUtilsNew.ts
@@ -1,46 +1,50 @@
-// Build valid input bindings for a transition (only real input arcs considered here).
-// This function returns an array of binding objects mapping variableName -> array[value].
-// These mapped arrays contain all values for one variableName that can be used to fire once.
+// Binding: Maps variableName (e.g. O) to concrete value from token (e.g. Order_1)
+type Binding = { [variableName: string]: string };
 
-import { is } from "../util/Util";
+// ArcBindingCandidates: All possible bindings for one arc
+type ArcBindingCandidates = Array<Binding>;
 
-type TokenValue = {
-  dataClassId: string;
-  value: string;
-};
+// ValidBindings: All valid complete bindings for a transition
+// (chooses one binding from each ArcBindingsCandidates per arc)
+type ValidBindings = Array<Binding>;
 
-type DataClassInfo = {
+interface DataClassInfo {
   isVariable: boolean;
   label: string;
-  tokenValues: Array<string>;
-};
+  tokenValues: Array<TokenValue["value"]>;
+}
 
-type ArcPlaceInfo = {
+interface ArcPlaceInfo {
   arcId: string;
   placeId: string;
   token: Array<TokenValue>;
   isInhibitorArc: boolean;
   isLinkingPlace: boolean;
+  // variableClass: DataClassInfo | undefined; // TODO: needed?
   dataClassInfoDict: {
     [dataClassId: string]: DataClassInfo;
   };
-};
+}
 
-function buildArcPlaceInfo(arc: any) {
-  const arcId = arc.businessObject.id;
-  const place = arc.businessObject.source;
-  const placeId = place.id;
-  const isInhibitorArc = arc.businessObject.isInhibitorArc || false;
+interface ArcPlaceInfoDict {
+  [arcId: string]: ArcPlaceInfo;
+}
+
+function buildArcPlaceInfo(arc: Arc): ArcPlaceInfo {
+  const place: Place = arc.businessObject.source as Place;
+  const isInhibitorArc: boolean = arc.businessObject.isInhibitorArc || false;
 
   const dataClassInfoDict: {
     [dataClassId: string]: DataClassInfo;
   } = {};
 
-  const marking = place.marking || [];
+  const marking = place.marking ?? [];
+
   for (const token of marking) {
-    const tokenValues = token.values || [];
+    const tokenValues = token.values ?? [];
     for (const { dataClass, value } of tokenValues) {
       if (!dataClass) continue;
+
       if (!dataClassInfoDict[dataClass.id]) {
         dataClassInfoDict[dataClass.id] = {
           isVariable: false,
@@ -48,43 +52,33 @@ function buildArcPlaceInfo(arc: any) {
           tokenValues: [],
         };
       }
-      const classInfo = dataClassInfoDict[dataClass.id];
-      if (classInfo && !classInfo.tokenValues.includes(value)) {
-        classInfo.tokenValues.push(value);
+
+      if (!dataClassInfoDict[dataClass.id].tokenValues.includes(value)) {
+        dataClassInfoDict[dataClass.id].tokenValues.push(value);
       }
     }
   }
 
-  const inscription = arc.businessObject.inscription;
-  if (inscription && inscription.inscriptionElements) {
-    for (const element of inscription.inscriptionElements) {
-      const dataClass = element.dataClass;
-      if (dataClass && dataClassInfoDict[dataClass.id]) {
-        const classInfo = dataClassInfoDict[dataClass.id];
-        if (classInfo) {
-          classInfo.isVariable = arc.businessObject.variableType === dataClass;
-          classInfo.label = element.variableName;
-        }
-      }
+  const inscriptionElements =
+    arc.businessObject.inscription?.inscriptionElements ?? [];
+  for (const element of inscriptionElements) {
+    const dataClass = element.dataClass;
+    const dataClassInfo = dataClassInfoDict[dataClass.id];
+    if (dataClass?.id && dataClassInfo) {
+      dataClassInfo.isVariable = arc.businessObject.variableType === dataClass;
+      dataClassInfo.label = element.variableName;
     }
   }
 
-  const customMarking: Array<TokenValue> = marking
-    .map((token: any) => {
-      const tokenValues: Array<TokenValue> = [];
-      const values = token.values || [];
-      for (const { dataClass, value } of values) {
-        if (dataClass) {
-          tokenValues.push({ dataClassId: dataClass.id, value });
-        }
-      }
-      return tokenValues;
-    })
-    .flat();
+  const customMarking: Array<TokenValue> = marking.flatMap((token) =>
+    (token.values ?? [])
+      .filter(({ dataClass }) => dataClass)
+      .map(({ dataClass, value }) => ({ dataClass, value })),
+  ); // Transforms marking of token into Array of TokenValues
 
   return {
-    arcId,
-    placeId,
+    arcId: arc.id,
+    placeId: place.id,
     token: customMarking,
     isInhibitorArc,
     isLinkingPlace: Object.keys(dataClassInfoDict).length > 1,
@@ -92,64 +86,174 @@ function buildArcPlaceInfo(arc: any) {
   };
 }
 
-// TODO: transition invalid if input arc is variable and matching output arc is not variable and vice versa
-// Returns true if one of outgoing arcs doesn't match incoming arc and is not generating
-function isInvalidDueToOutputs(incomingArcs: any[], outgoingArcs: any[]) {
-  const realInputVarNames = new Set();
-  incomingArcs.forEach((arc) => {
-    if (arc.businessObject.isInhibitorArc) return;
-    const labels = (
-      arc.businessObject?.inscription?.inscriptionElements || []
-    ).map((el: any) => el.variableName);
-    labels.forEach((l: string) => realInputVarNames.add(l));
-  });
+// Returns true if any output arc has a non-generated variable that was not defined in the input arcs
+function hasUnboundOutputVariables(
+  incomingArcs: Array<Arc>,
+  outgoingArcs: Array<Arc>,
+): boolean {
+  const inputVariableNames = new Set<string>(
+    incomingArcs
+      .filter((arc) => !arc.businessObject.isInhibitorArc) // exclude inhibitor arcs
+      .flatMap(
+        (arc: Arc) => arc.businessObject.inscription?.inscriptionElements || [],
+      )
+      .map((el: any) => el.variableName),
+  );
 
-  for (const arc of outgoingArcs) {
-    if (arc.businessObject.isInhibitorArc) continue;
+  return outgoingArcs
+    .filter((arc) => !arc.businessObject.isInhibitorArc)
+    .some((arc) => {
+      const inscriptionElements =
+        arc.businessObject.inscription?.inscriptionElements || [];
+      return inscriptionElements.some(
+        (el: InscriptionElement) =>
+          !el.isGenerated && !inputVariableNames.has(el.variableName),
+      );
+    });
+}
 
-    const elements = arc.businessObject.inscription?.inscriptionElements || [];
-    const hasUnboundVariable = elements.some(
-      (el: any) => !el.isGenerated && !realInputVarNames.has(el.variableName),
-    ); // unbound = variable not generated and not as input
-
-    if (hasUnboundVariable) return true;
-  }
-
+function hasMismatchedVariableTypes(
+  incomingArcs: Array<Arc>,
+  outgoingArcs: Array<Arc>,
+): boolean {
+  // TODO: implement mismatched variables types check
   return false;
 }
 
-export function getValidInputBindings(transition: any) {
-  if (isInvalidDueToOutputs(transition.incoming, transition.outgoing)) {
+// Check if all non-inhibitor arcs have at least one available token
+function hasAvailableTokensForAllArcs(
+  arcPlaceInfoDict: ArcPlaceInfoDict,
+): boolean {
+  return Object.values(arcPlaceInfoDict).every(
+    (arcPlaceInfo) =>
+      arcPlaceInfo.isInhibitorArc || arcPlaceInfo.token.length > 0,
+  );
+}
+
+function getInhibitorTokens(
+  arcPlaceInfoDict: ArcPlaceInfoDict,
+): Array<TokenValue> {
+  const inhibitorTokens: Array<TokenValue> = [];
+
+  for (const arcPlaceInfo of Object.values(arcPlaceInfoDict)) {
+    if (!arcPlaceInfo.isInhibitorArc) continue;
+
+    inhibitorTokens.push(...arcPlaceInfo.token);
+  }
+
+  return inhibitorTokens;
+}
+
+function cartesianProduct(arrays: Array<ArcBindingCandidates>): ValidBindings {
+  if (arrays.length === 0) return [{}]; // empty binding;
+
+  return arrays.reduce((acc, current) =>
+    acc.flatMap((product) => current.map((item) => ({ ...product, ...item }))),
+  );
+}
+
+function tokensEqual(tokenA: TokenValue, tokenB: TokenValue): boolean {
+  return (
+    tokenA.dataClass.id === tokenB.dataClass.id && tokenA.value === tokenB.value
+  );
+}
+
+function isTokenBlockedByInhibitor(
+  token: TokenValue,
+  inhibitorTokens: Array<TokenValue>,
+): boolean {
+  return inhibitorTokens.some((inhibitorToken) =>
+    tokensEqual(token, inhibitorToken),
+  );
+}
+
+function buildBindingFromToken(
+  token: TokenValue,
+  dataClassInfoDict: { [dataClassId: string]: DataClassInfo },
+): Binding {
+  const binding: Binding = {};
+  for (const [dataClassId, dataClassInfo] of Object.entries(
+    dataClassInfoDict,
+  )) {
+    if (dataClassInfo.label && token.dataClass.id === dataClassId) {
+      binding[dataClassInfo.label] = token.value;
+    }
+  }
+  return binding;
+}
+
+export function getValidInputBindings(transition: Transition): ValidBindings {
+  if (hasUnboundOutputVariables(transition.incoming, transition.outgoing)) {
+    console.log("Early return: unbound output variables");
+    return [];
+  }
+
+  if (hasMismatchedVariableTypes(transition.incoming, transition.outgoing)) {
+    console.log("Early return: mismatched variable types");
     return [];
   }
 
   // If no incoming arcs, transition is always enabled
   if (transition.incoming.length === 0) {
-    return [[]]; // For consistency, return array with one empty binding
+    console.log("Early return: no incoming arcs");
+    return [{}]; // For consistency, return array with one empty binding
   }
 
-  const arcPlaceInfoMap: { [arcId: string]: ArcPlaceInfo } = {};
-  for (const arc of transition.incoming) {
-    arcPlaceInfoMap[arc.id] = buildArcPlaceInfo(arc);
-  }
+  const arcPlaceInfoDict: ArcPlaceInfoDict = Object.fromEntries(
+    transition.incoming.map((arc) => [arc.id, buildArcPlaceInfo(arc)]),
+  );
 
   // For each arcPlaceInfo, check if there are tokens available, otherwise return no bindings
-  if (
-    !Object.values(arcPlaceInfoMap).every(
-      (arcPlaceInfo) => arcPlaceInfo.token.length > 0,
-    )
-  ) {
+  if (!hasAvailableTokensForAllArcs(arcPlaceInfoDict)) {
+    console.log("Early return: missing tokens in non-inhibitor arcs");
     return [];
   }
 
-  const arcBindings: any = [];
-  for (const arcPlaceInfo of Object.values(arcPlaceInfoMap)) {
-  }
+  const inhibitorTokens = getInhibitorTokens(arcPlaceInfoDict);
 
-  return arcBindings;
+  // Build binding candidates for each non-inhibitor arc
+  const allArcBindingCandidates: Array<ArcBindingCandidates> = [];
+
+  for (const arcPlaceInfo of Object.values(arcPlaceInfoDict)) {
+    if (arcPlaceInfo.isInhibitorArc) continue;
+
+    // More concise version than down below where we iterate over all tokens
+    // const bindingCandidates: ArcBindingCandidates = arcPlaceInfo.token
+    //   .filter((token) => !isTokenBlockedByInhibitor(token, inhibitorTokens)) // Skip tokens blocked by inhibitor arcs
+    //   .map((token) =>
+    //     buildBindingFromToken(token, arcPlaceInfo.dataClassInfoDict),
+    //   );
+
+    const bindingCandidates: ArcBindingCandidates = [];
+    for (const token of arcPlaceInfo.token) {
+      // Skip tokens blocked by inhibitor arcs
+      // TODO: In the discussed example it should print that token Item_1 and (Order_1, Item_1) are blocked by inhibitor token Item_1
+      // Currently only prints token Item_1 and Item_1
+      if (isTokenBlockedByInhibitor(token, inhibitorTokens)) {
+        console.log("Skipping token blocked by inhibitor arc:", token);
+        continue;
+      }
+
+      // Build binding from token
+      const binding: Binding = buildBindingFromToken(
+        token,
+        arcPlaceInfo.dataClassInfoDict,
+      );
+      bindingCandidates.push(binding);
+    } // End for token
+
+    if (bindingCandidates.length === 0) {
+      return [];
+    }
+
+    allArcBindingCandidates.push(bindingCandidates);
+  } // End for arcPlaceInfo
+
+  console.log("allArcBindingCandidates", allArcBindingCandidates);
+  return cartesianProduct(allArcBindingCandidates);
 }
 
-export function transitionIsEnabled(transition: any): boolean {
+export function transitionIsEnabled(transition: Transition): boolean {
   const bindings = getValidInputBindings(transition);
   return !!bindings.length;
 }
